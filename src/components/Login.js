84a7801dfa0a8b60b8fd24440493731f82bfb575
Seth Moffat <sethmoffat1@gmail.com>
2022-04-30 15:52:41 -0600

    Barely started

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <h1>Login</h1>
+
+        <p>Name</p>
+        <input></input>
+        <br/>
+        <p>Password</p>
+        <input/>
+        <br/>
+        <button>Login</button>
+        <br/> <br/>
+
+        <p>Don't have an account</p>
+        <button>Click Here</button>
+    </div>
+  )
+}
+
+export default Login
